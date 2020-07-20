c81d58d166de2b74aa294a2990fee12c19497e8c
usetelni <thiago.usetelni@gmail.com>
2020-07-19 21:00:43 -0300

    initial commit

---
+++ src/main/java/br/com/petz/PetzTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.petz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetzTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetzTesteApplication.class, args);
+	}
+
+}
